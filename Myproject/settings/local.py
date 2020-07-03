0e13ec3ac54ff16d70701edc96e433b424ac7966
Luciferiitr <ipandey@cs.iitr.ac.in>
2020-07-03 21:47:14 +0530

    collectstatic

--- Myproject/settings/local.py
+++ Myproject/settings/local.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -133,7 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = STATIC_DIR
+STATICFILES_DIR = [STATIC_DIR,]
 
 CORS_REPLACE_HTTPS_REFERER      = False
 HOST_SCHEME                     = "http://"
